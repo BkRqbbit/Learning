cc6f5b49770da9b5994eb75d18059fefc405fffe
BkRqbbit <bladyalvarez115dev@gmail.com>
2024-05-17 14:36:42 -0600

    Print variables in DOM

---
+++ JavaScript/005-variables-in-dom/index.js
@@ -0,0 +1,8 @@
+let name_element = document.getElementById("name");
+let age_element = document.getElementById("age");
+
+let name = "BkRqbbit";
+let age = 18;
+
+name_element.textContent = `Your name is ${name}`;
+age_element.textContent = `You are ${age} years old!`;
