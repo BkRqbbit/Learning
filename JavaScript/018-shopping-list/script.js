f319301b1d0bd8c97760271af7cea35968fe9681
BkRqbbit <bladyalvarez115dev@gmail.com>
2024-06-26 15:53:53 -0600

    Practice example (Arrays)

---
+++ JavaScript/018-shopping-list/script.js
@@ -0,0 +1,9 @@
+let shopping = ["Milk", "Bread", "Apples"];
+let size = shopping.length;
+
+console.log("The size of the list is: " + size);
+console.log("Shopping list: " + shopping);
+
+shopping[1] = "Bananas";
+
+console.log("Shopping list: " + shopping);
