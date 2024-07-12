6d4e163dcb833d0b3273730c45102034ef72e1f5
BkRqbbit <bladyalvarez115dev@gmail.com>
2024-07-12 17:12:59 -0600

    Multiplication of two numbers

---
+++ C++/004-multiplication/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+    int x, y;
+
+    std::cout << "Enter x: ";
+    std::cin >> x;
+
+    std::cout << "Enter y: ";
+    std::cin >> y;
+
+    std::cout << "The result of the multiplication is: " << x * y << std::endl;
+    
+    return 0;
+}
