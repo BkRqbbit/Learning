5e91f8e38e8136fcf24d9cee8868532ed5ea9826
BkRqbbit <bladyalvarez115dev@gmail.com>
2024-07-11 22:31:55 -0600

    Add two numbers

---
+++ C++/002-add-two-values/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    int x = 0;
+    int y = 0;
+    
+    std::cout << "Enter the value of x: ";
+    std::cin >> x;
+
+    std::cout << "Enter the value of y: ";
+    std::cin >> y;
+
+    std::cout << "The result of x + y is: " << x + y << std::endl;
+    
+    return 0;
+}
