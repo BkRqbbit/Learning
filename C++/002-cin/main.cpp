5385d9583fbf645b19dfe95a8be4351cf0e397da
BkRqbbit <bladyalvarez115dev@gmail.com>
2024-07-11 22:25:46 -0600

    Use of cin

---
+++ C++/002-cin/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(){
+    int age = 0;
+
+    std::cout << "Input your age: ";
+    std::cin >> age;
+
+    std::cout << "Age: " << age << std::endl;
+    
+    return 0;
+}
