4b4f7ee895ff7e157f08d3c7fe4f62e57935f111
BkRqbbit <bladyalvarez115dev@gmail.com>
2024-10-21 19:31:03 -0600

    Change data types, and limit decimals shown

--- C/the-c-programming-language/002-fahrenheit_to_celsius/main.c
+++ C/the-c-programming-language/002-fahrenheit_to_celsius/main.c
@@ -3,6 +3,8 @@
     Formula: (5/9) * (F-32)
 
     V2: Add formatted output
+    V3: Change values type (int -> float) for more precise results
+        Limit the number of decimal numbers shown
 */
 
 #include<stdio.h>
@@ -12,13 +14,12 @@
 #define STEP 20
 
 int main(){
-    int fahrenheit = LOW_LIMIT;
-    int celsius = 0;
+    float fahrenheit = LOW_LIMIT;
+    float celsius = 0;
 
     while( fahrenheit <= HIGH_LIMIT ){
         celsius = (5/9.0) * (fahrenheit-32);
-        printf("%3dF %3dC\n", fahrenheit, celsius);
-
+        printf("%6.2fF %6.2fC\n", fahrenheit, celsius);
         fahrenheit += STEP;
     }
 
